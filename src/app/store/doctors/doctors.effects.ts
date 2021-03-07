c0b3de6bf8a5e35327be85ee3dd746ee4e219a91
Mansour Alnasser <alnassre.it@gmail.com>
2021-03-07 17:55:01 +0300

    doctor loading spenner

--- src/app/store/doctors/doctors.effects.ts
+++ src/app/store/doctors/doctors.effects.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
 import { of } from 'rxjs';
 import { mergeMap, map, catchError } from 'rxjs/operators';
 import { DoctorsService } from 'src/app/services/doctors/doctors.service';
-import { setErrorMessage } from '../shared/shared.actions';
+import { AppState } from '../app.state';
+import { setErrorMessage, setLoadingSpinner } from '../shared/shared.actions';
 import { loadDoctors, loadDoctorsSuccess } from './doctors.actions';
 
 
@@ -13,6 +15,7 @@ export class DoctorEffects {
 
   constructor(
     private actions$: Actions,
+    private store: Store<AppState>,
     private doctorsService: DoctorsService
   ) { }
 
@@ -24,11 +27,14 @@ export class DoctorEffects {
         ofType(loadDoctors),
         // mergeMap or exhaustMap
         mergeMap((action) => {
+          this.store.dispatch(setLoadingSpinner({ status: true }));
           return this.doctorsService.getDoctors().pipe(
             map((doctors) => {
+              this.store.dispatch(setLoadingSpinner({ status: false }));
               return loadDoctorsSuccess({ doctors });
             }),
             catchError((error) => {
+              this.store.dispatch(setLoadingSpinner({ status: false }));
               return of(
                 setErrorMessage({ message: error.message })
               );
